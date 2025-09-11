7afd77194be3647fd4c514ba1d0e058311c2bf3a
ujjwalsangal-815 <ujjawalsangal815@gmail.com>
2025-09-11 21:36:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+print("Addititon:",x+y)
+print("subtraction:",x-y)
+print("multiplication:",x*y)
+print("Division:",x/y)
