b79e2df17b9da19249777d0bf88962da6ee70cf8
ujjawalsangal <ujjawalsangal@gmail.com>
2026-07-11 23:21:06 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,13 @@
+'''Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.'''
+
+
 x=int(input("Enter the first number:"))
 y=int(input("Enter the second number:"))
 
